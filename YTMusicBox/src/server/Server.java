07d20d5661f637c16f777c7068b32c69c7a9aa47
Mellich <mellich@gmx.net>
2015-01-23 13:42:41 +0100

    javadoc added

--- YTMusicBox/src/server/Server.java
+++ YTMusicBox/src/server/Server.java
@@ -1,10 +1,32 @@
 package server;
 
 public interface Server {
+	/**
+	 * starts the server. This method blocks the thread that is calling it until the server was shut down 
+	 * via the shutDown method!
+	 */
 	public void startUp();
-	public void shutDown();
-	public String getWorkingDir();
-	public int getCurrentClientCount();
-	public int getCurrentPlayerCount();
 	
+	/**
+	 * shuts down the server
+	 */
+	public void shutDown();
+	
+	/**
+	 * gets the current working directory as a absolute path
+	 * @return the current working directory
+	 */
+	public String getWorkingDir();
+	
+	/**
+	 * gets the current count of clients that are connected to the server and declared themselves as notifiable
+	 * @return count of clients
+	 */
+	public int getCurrentClientCount();
+	
+	/**
+	 * gets the count of current connected players to the server
+	 * @return count of connected players
+	 */
+	public int getCurrentPlayerCount();	
 }
