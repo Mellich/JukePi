cf0f93dfb5afa5bc5a0fe9a4a0969ce4516708c5
Haeldeus <nordein.vail@yahoo.de>
2015-01-26 15:32:35 +0100

    Small Change
    Unnecessary Output deleted.

--- Network/src/client/serverconnection/functionality/YTJBLowLevelServerConnection.java
+++ Network/src/client/serverconnection/functionality/YTJBLowLevelServerConnection.java
@@ -153,7 +153,6 @@ public class YTJBLowLevelServerConnection implements LowLevelServerConnection {
 						}
 					}
 				} catch (InterruptedException e) {
-					System.out.println("CheckThread beendet");
 				}
 				close();
 			}
