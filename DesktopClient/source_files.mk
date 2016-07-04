364d957ffeb2d323306a868ad3f6f6ed8dd19f8e
Marius Meyer <marius-meyer@hotmail.com>
2016-07-04 16:29:04 +0200

    changed makefile for the client to search dynamically for .java files

--- DesktopClient/source_files.mk
+++ DesktopClient/source_files.mk
@@ -1,76 +1,5 @@
-CLIENT_SRC_FILES = \
-$(CLIENT_DIR)$(INPUT_DIR)/util/SupportedSites.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/TextFieldListener.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/OptionsTableClickListener.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/PopUpMenu.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/ShowLabelThread.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/IO.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/JMultilineLabel.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/TextTransfer.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/TablePopUpMenu.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/TableRenderer.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/layouts/DisplayGaplistsLayout.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/layouts/NewClientLayout.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/layouts/LowClientLayout.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/layouts/LoginLayout.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/layouts/ClientLayout.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/TablePopClickListener.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/PopClickListener.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/tasks/SetGaplistTask.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/tasks/SetWishlistTask.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/tasks/SetSavedGaplistsTask.java \
-$(CLIENT_DIR)$(INPUT_DIR)/util/tasks/SetContentTask.java \
-$(CLIENT_DIR)$(INPUT_DIR)/listener/UDPListener.java \
-$(CLIENT_DIR)$(INPUT_DIR)/windows/AckWindow.java \
-$(CLIENT_DIR)$(INPUT_DIR)/windows/MainWindow.java \
-$(CLIENT_DIR)$(INPUT_DIR)/windows/NewListWindow.java \
-$(CLIENT_DIR)$(INPUT_DIR)/windows/LowClientWindow.java \
-$(CLIENT_DIR)$(INPUT_DIR)/windows/Window.java \
-$(CLIENT_DIR)$(INPUT_DIR)/windows/DebugWindow.java \
-$(CLIENT_DIR)$(INPUT_DIR)/windows/LoginWindow.java \
-$(CLIENT_DIR)$(INPUT_DIR)/windows/DisplayGaplistsWindow.java \
-$(CLIENT_DIR)$(INPUT_DIR)/windows/SetPasswordWindow.java \
-$(CLIENT_DIR)$(INPUT_DIR)/windows/OptionsWindow.java \
-$(CLIENT_DIR)$(INPUT_DIR)/windows/PasswordWindow.java \
-$(CLIENT_DIR)$(INPUT_DIR)/connection/Collector.java \
-$(CLIENT_DIR)$(INPUT_DIR)/start/ClientStarter.java 
-
-CLIENT_CLASS_FILES = \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/SupportedSites.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/TextFieldListener.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/OptionsTableClickListener.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/PopUpMenu.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/ShowLabelThread.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/IO.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/JMultilineLabel.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/TextTransfer.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/TablePopUpMenu.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/TableRenderer.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/layouts/DisplayGaplistsLayout.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/layouts/NewClientLayout.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/layouts/LowClientLayout.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/layouts/LoginLayout.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/layouts/ClientLayout.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/TablePopClickListener.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/PopClickListener.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/tasks/SetGaplistTask.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/tasks/SetWishlistTask.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/tasks/SetSavedGaplistsTask.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/util/tasks/SetContentTask.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/listener/UDPListener.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/windows/AckWindow.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/windows/MainWindow.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/windows/NewListWindow.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/windows/LowClientWindow.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/windows/Window.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/windows/DebugWindow.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/windows/LoginWindow.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/windows/DisplayGaplistsWindow.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/windows/SetPasswordWindow.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/windows/OptionsWindow.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/windows/PasswordWindow.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/connection/Collector.class \
-$(CLIENT_DIR)$(OUTPUT_DIR)/start/ClientStarter.class 
+CLIENT_SOURCE_FILES = $(shell find $(CLIENT_DIR)$(INPUT_DIR) -type f \( -name \*.java ! -name \*package-info.java \))
+CLIENT_CLASS_FILES = $(subst $(INPUT_DIR)/,$(OUTPUT_DIR)/, $(CLIENT_SOURCE_FILES:%.java=%.class))
 
 CLIENT_ARTWORK = \
 $(CLIENT_DIR)$(OUTPUT_DIR)/resources/skip.png \
@@ -93,7 +22,7 @@ $(CLIENT_DIR)$(OUTPUT_DIR)/%.class: $(CLIENT_DIR)$(INPUT_DIR)/%.java
 	@echo "Creating output folder: $(CLIENT_DIR)$(OUTPUT_DIR)"
 	@$(MD) $(CLIENT_DIR)$(OUTPUT_DIR)
 	@echo "Compiling JukePi client"
-	@$(JC) $(JFLAGS) -d $(CLIENT_DIR)$(OUTPUT_DIR) -classpath $(NETWORK_DIR)$(OUTPUT_DIR)$::$(SERVER_DIR)$(OUTPUT_DIR) $(CLIENT_SRC_FILES)
+	$(JC) $(JFLAGS) -d $(CLIENT_DIR)$(OUTPUT_DIR) -classpath $(NETWORK_DIR)$(OUTPUT_DIR)$::$(SERVER_DIR)$(OUTPUT_DIR) $(shell find $(CLIENT_DIR)$(INPUT_DIR) -name \*.java)
 
 
 
